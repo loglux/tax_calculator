fcc1efb196766c708ea3ef1b1dc28ae8493a1464
loglux <call2vlad@outlook.com>
2024-08-23 17:49:13 +0100

    added azure.py with azure hosting settings

--- tax_calculator/azure.py
+++ tax_calculator/azure.py
@@ -1,12 +1,13 @@
-from .settings import *
 import os
+from .settings import *
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # Enables whitenoise for serving static files
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -22,8 +23,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Set the SECRET_KEY with one read from environmental variables by adding the following code
-# SECRET_KEY = os.getenv('SECRET_KEY')
-
 DEBUG = False
-
