6fcdd912ea25bc7e577bf8940fc0e1476d2acd25
loglux <call2vlad@outlook.com>
2024-08-23 18:51:26 +0100

    added azure.py with azure hosting settings

--- tax_calculator/settings.py
+++ tax_calculator/settings.py
@@ -12,14 +12,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Load the Azure settings if running on Azure
 # import os
 if 'WEBSITE_HOSTNAME' in os.environ: # Running on Azure
     from .azure import *
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
