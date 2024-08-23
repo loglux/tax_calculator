e08ddc8bb0e96cb891a339ef3e24f4c7a6167ef4
loglux <call2vlad@outlook.com>
2024-08-23 18:51:26 +0100

    added azure.py with azure hosting settings

--- tax_calculator/azure.py
+++ tax_calculator/azure.py
@@ -1,7 +1,7 @@
 import os
 from .settings import *
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
